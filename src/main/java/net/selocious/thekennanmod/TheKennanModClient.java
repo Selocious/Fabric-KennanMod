f007fbf8bd091ae92bbaec8f9ee749bc2094da09
Selocious <shaunduthie07@gmail.com>
2025-02-06 23:49:40 +0100

    Initial commit

---
+++ src/main/java/net/selocious/thekennanmod/TheKennanModClient.java
@@ -0,0 +1,10 @@
+package net.selocious.thekennanmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheKennanModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
