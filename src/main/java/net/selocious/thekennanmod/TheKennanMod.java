8b8790907fdd4c9e791d70b2a5f87676743a96f1
Selocious <shaunduthie07@gmail.com>
2025-02-07 02:41:51 +0100

    ds

--- src/main/java/net/selocious/thekennanmod/TheKennanMod.java
+++ src/main/java/net/selocious/thekennanmod/TheKennanMod.java
@@ -2,6 +2,7 @@ package net.selocious.thekennanmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.selocious.thekennanmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class TheKennanMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/selocious/thekennanmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.selocious.thekennanmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.selocious.thekennanmod.TheKennanMod;
+
+public class ModItems {
+    public static final Item BATTERY = registerItem("battery", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TheKennanMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TheKennanMod.LOGGER.info("Registering Mod Items for " + TheKennanMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(BATTERY);
+        });
+    }
+}
