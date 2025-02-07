fc44f30e0e0d749a36be9b7b6e2658fa29753ba3
Selocious <shaunduthie07@gmail.com>
2025-02-07 02:54:46 +0100

    Added Batteries

--- src/main/java/net/selocious/thekennanmod/item/ModItems.java
+++ src/main/java/net/selocious/thekennanmod/item/ModItems.java
@@ -5,11 +5,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.selocious.thekennanmod.TheKennanMod;
 
 public class ModItems {
-    public static final Item BATTERY = registerItem("battery", new Item(new Item.Settings()));
+    public static final Item BATTERY = registerItem("battery",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TheKennanMod.MOD_ID,"battery")))));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TheKennanMod.MOD_ID, name), item);
